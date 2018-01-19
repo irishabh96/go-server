b3570503abd24544219fe9794d770327524acd93
Rishabh Bhatia <irishabh96@gmail.com>
2018-01-20 02:36:43 +0530

    fix

--- src/services/facebook/index.js
+++ src/services/facebook/index.js
@@ -1,23 +1,17 @@
-import crypto from 'crypto'
-import request from 'request-promise'
-import { fbAppSecret } from '../../config'
-
-const getAppSecretProof = (accessToken) =>
-  crypto.createHmac('sha256', fbAppSecret).update(accessToken).digest('hex')
+import request from 'request-promise';
 
 export const getUser = (accessToken) =>
-  request({
-    uri: 'https://graph.facebook.com/me',
-    json: true,
-    qs: {
-      access_token: accessToken,
-      appsecret_proof: getAppSecretProof(accessToken),
-      fields: 'id, name, email'
-    }
-  }).then(({ id, name, email }) => ({
-    service: 'facebook',
-    picture: `https://graph.facebook.com/${id}/picture?type=normal`,
-    id,
-    name,
-    email
-  }))
+	request({
+		uri: 'https://graph.facebook.com/me',
+		json: true,
+		qs: {
+			access_token: accessToken,
+			fields: 'id, name, email, picture'
+		}
+	}).then(({ id, name, email, picture }) => ({
+		service: 'facebook',
+		picture: picture.data.url,
+		id,
+		name,
+		email
+	}));
